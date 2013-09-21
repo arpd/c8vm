4e3b1b55b794bcbda7886c1932a06b8544166aba
Benji Foster <benji@ripe.li>
2013-09-21 17:48:26 +0100

    Added load function, and moved usage out.

--- src/c8vm.cpp
+++ src/c8vm.cpp
@@ -1,18 +1,53 @@
 #include "c8.h"
-#include "c8_config.h"
+#include "c8_config.h" // CMake configuration file
 #include <iostream>
+#include <fstream>
+#include <vector>
 
 using namespace std;
 
+// ----------------------------------------------------------------------------
+void print_usage() {
+    string prog("c8vm");
+    cout << prog << " version "
+         << c8vm_VERSION_MAJOR << "." << c8vm_VERSION_MINOR
+         << endl;
+    cout << "usage: " << prog << " [[OPT1] [OPT2]] ..." << endl;
+}
+
+// ----------------------------------------------------------------------------
+string& load_binary(string& out, char* path) {
+    ifstream infs(path, ios::binary);
+    if (infs) {
+        /* get filesize so we don't have to read char by char */
+        infs.seekg(0, ios::end);
+        streampos file_len = infs.tellg();
+        infs.seekg(0, ios::beg);
+
+        /* use a vector as the buffer, as they are exception safe and will
+           be tidied up correctly */
+        vector<char> buf(file_len);
+        infs.read(&buf[0], file_len);
+        out = string(buf.begin(), buf.end());
+    }
+    return out;
+}
+
+// ----------------------------------------------------------------------------
 int main(int argc, char** argv) {
     if (argc < 2) {
-        cout << argv[0] << " version "
-             << c8vm_VERSION_MAJOR << "." << c8vm_VERSION_MINOR
-             << endl;
-        cout << "usage: " << argv[0] << " [[OPT1] [OPT2]] ..." << endl;
-
+        print_usage();
+        return 0;
     }
+
+    // load the binary
+    string bin = "";
+    bin = load_binary(bin, argv[1]);
+
+    // init vm
     C8VM vm;
+    vm.load(bin);
+    vm.start();
 
     return 0;
 }
